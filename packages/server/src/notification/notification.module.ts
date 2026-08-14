edbc5c2b85a90f6e09c79f69ec14e97d2ce767eb
niobium617 <ljkaixhxx@outlook.com>
2026-08-14 23:19:25 +0800

    fix: make NotificationModule @Global so AdminService can inject NotificationService
    Error: Nest can't resolve dependencies of the AdminService (PrismaService, ?)

--- packages/server/src/notification/notification.module.ts
+++ packages/server/src/notification/notification.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 
+@Global()
 @Module({
   controllers: [NotificationController],
   providers: [NotificationService],
